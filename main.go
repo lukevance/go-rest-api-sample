62ebe6b7652fccf2c9229d46f13aa9b5a3726416
lvance <lvance@twilio.com>
2021-12-06 17:41:43 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type pizza struct {
+	ID    string  `json:"id"`
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/pizzas", getPizzas)
+	router.POST("/pizzas", postPizzas)
+
+	router.Run("localhost:8080")
+}
+
+// seed data for pizzas
+var pizzas = []pizza{
+	{ID: "1", Name: "Cheese", Price: 9.99},
+	{ID: "2", Name: "Pepperoni", Price: 11.99},
+}
+
+// getPizzas responds with the list of all pizzas
+func getPizzas(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, pizzas)
+}
+
+// postPizzas adds a pizza to the menu from the request
+func postPizzas(c *gin.Context) {
+	var newPizza pizza
+
+	//bind the received json to newPizza
+	if err := c.BindJSON(&newPizza); err != nil {
+		return
+	}
+
+	//add pizza to our in memory object
+	pizzas = append(pizzas, newPizza)
+	c.IndentedJSON(http.StatusCreated, newPizza)
+}
